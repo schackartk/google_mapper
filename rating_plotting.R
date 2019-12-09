3d5473a3e0e91e818bfd634c9ef597e308bf1339
schackartk <schackartk@email.arizona.edu>
2019-12-09 08:57:58 -0700

    More code tidying

--- rating_plotting.R
+++ rating_plotting.R
@@ -9,7 +9,8 @@ register_google(key = google_key)
 # Import the data and filter out columns that are not useful
 ratings <- fromJSON("maps_data/Reviews.json") %>% 
   as_tibble() %>% flatten() %>% as_tibble() %>%
-  select(-type, -features.type, -features.geometry.coordinates, -features.geometry.type, -`features.properties.Location.Country Code`) 
+  select(-type, -features.type, -features.geometry.coordinates, 
+         -features.geometry.type, -`features.properties.Location.Country Code`) 
 
 # Rename columns to make more sense
 colnames(ratings)[1] <- "maps_url"
@@ -21,14 +22,15 @@ colnames(ratings)[6] <- "business_name"
 colnames(ratings)[7] <- "latitude"
 colnames(ratings)[8] <- "longitude"
 
-# Clean up address
-state.zip <- str_match(ratings$address,"([[:alpha:]]{2})[[:space:]]([[:digit:]]{5})")
+# Parse out parts of the address column using regular expressions via stringr::str_match
+addresses <- str_match(ratings$address,
+                       "[[:digit:]]+[[:space:]][[:alpha:]]{1}[[:space:]][[:alnum:][:space:]]+") # e.g. "21 N Squaw Creek Dr"
+cities <- str_match(ratings$address,"[,][[:space:]][[:alpha:]]+[,]") %>% # e.g. ", Bronx,"
+  gsub(pattern = ",",replacement = "") %>% trimws() %>% as.vector()
+state.zip <- str_match(ratings$address,"([[:alpha:]]{2})[[:space:]]([[:digit:]]{5})") # e.g. "NY 10469"
   states <- state.zip[,2]
   zips <- state.zip[,3]
-addresses <- str_match(ratings$address,"[[:digit:]]+[[:space:]][[:alpha:]]{1}[[:space:]][[:alnum:][:space:]]+")
-cities <- str_match(ratings$address,"[,][[:space:]][[:alpha:]]+[,]") %>%
-  gsub(pattern = ",",replacement = "") %>% trimws() %>% as.vector()
-countries <- str_match(ratings$address,"[[:alpha:][:space:]]+$") %>%
+countries <- str_match(ratings$address,"[[:alpha:][:space:]]+$") %>% # e.g. "United States" (anchored to end)
   gsub(pattern = "USA", replacement = "United States") %>% trimws()
 
 # Add in the columns of the parsed address column
@@ -38,23 +40,24 @@ ratings <- ratings %>%
 # Remove the old, unparsed, address column
 ratings <- ratings %>% select(-address)
 
-ratings_of_interest <- ratings[ratings$cities == "Tucson",]
+# Select the city to use 
+ratings_of_interest <- ratings[ratings$cities == "Tucson",] # This part is well suited for a shiny app, may change
 
 ratings_of_interest$latitude <- as.double(ratings_of_interest$latitude)
 ratings_of_interest$longitude <- as.double(ratings_of_interest$longitude)
 
 # Calculate boundaries for locaitons
-bounding_box <- c(min(ratings_of_interest$longitude, na.rm = TRUE)-0.05, # Left
-                 min(ratings_of_interest$latitude, na.rm = TRUE)-0.05, # Bottom
-                 max(ratings_of_interest$longitude, na.rm = TRUE)+0.05, # Right
-                 max(ratings_of_interest$latitude, na.rm = TRUE)+0.05 # Top
+buffer <- 0.05
+bounding_box <- c(min(ratings_of_interest$longitude, na.rm = TRUE) - buffer, # Left
+                 min(ratings_of_interest$latitude, na.rm = TRUE) - buffer,   # Bottom
+                 max(ratings_of_interest$longitude, na.rm = TRUE) + buffer,  # Right
+                 max(ratings_of_interest$latitude, na.rm = TRUE) + buffer    # Top
                  )
 
 # Get map from google based on location boundaries
 sq_map2 <-
   get_map(
     bounding_box,
-    maptype = "satellite",
     source = "google",
     zoom = 10
   )
