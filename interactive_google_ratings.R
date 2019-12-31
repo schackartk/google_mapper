9b3da280a9de4a886b229121223c60ab3d91eaf0
Kenny's Spectre <schackartk@email.arizona.edu>
2019-12-30 19:17:41 -0700

    Working interactively with my data

--- interactive_google_ratings.R
+++ interactive_google_ratings.R
@@ -53,6 +53,9 @@ ratings <- ratings %>%
 # Remove the old, unparsed, address column
 ratings <- ratings %>% select(-address)
 
+# Convert latitude and longitude to numeric type
+ratings$latitude <- as.numeric(ratings$latitude)
+ratings$longitude <- as.numeric((ratings$longitude))
 
 # Shiny Application -------------------------------------------------------
 
@@ -60,35 +63,26 @@ ui <- bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   leafletOutput("map", width = "100%", height = "100%"),
   absolutePanel(top = 10, right = 10,
-                sliderInput("range", "Magnitudes", min(quakes$mag), max(quakes$mag),
-                            value = range(quakes$mag), step = 0.1
-                ),
                 selectInput("colors", "Color Scheme",
                             rownames(subset(brewer.pal.info, category %in% c("seq", "div")))
-                ),
-                checkboxInput("legend", "Show legend", TRUE)
+                )
   )
 )
 
 server <- function(input, output, session) {
   
-  # Reactive expression for the data subsetted to what the user selected
-  filteredData <- reactive({
-    quakes[quakes$mag >= input$range[1] & quakes$mag <= input$range[2],]
-  })
-  
   # This reactive expression represents the palette function,
   # which changes as the user makes selections in UI.
   colorpal <- reactive({
-    colorNumeric(input$colors, quakes$mag)
+    colorFactor(input$colors, ratings$rating)
   })
   
   output$map <- renderLeaflet({
     # Use leaflet() here, and only include aspects of the map that
     # won't need to change dynamically (at least, not unless the
     # entire map is being torn down and recreated).
-    leaflet(quakes) %>% addTiles() %>%
-      fitBounds(~min(long), ~min(lat), ~max(long), ~max(lat))
+    leaflet(ratings) %>% addTiles() %>% 
+      fitBounds(~min(longitude), ~min(latitude), ~max(longitude), ~max(latitude))
   })
   
   # Incremental changes to the map (in this case, replacing the
@@ -98,26 +92,24 @@ server <- function(input, output, session) {
   observe({
     pal <- colorpal()
     
-    leafletProxy("map", data = filteredData()) %>%
+    leafletProxy("map", data = ratings) %>%
       clearShapes() %>%
-      addCircles(radius = ~10^mag/10, weight = 1, color = "#777777",
-                 fillColor = ~pal(mag), fillOpacity = 0.7, popup = ~paste(mag)
+      addCircles(radius = 500, weight = 1, color = "#777777",
+                 fillColor = ~pal(rating), fillOpacity = 0.7, popup = ~paste(rating)
       )
   })
   
   # Use a separate observer to recreate the legend as needed.
   observe({
-    proxy <- leafletProxy("map", data = quakes)
+    proxy <- leafletProxy("map", data = ratings)
     
     # Remove any existing legend, and only if the legend is
     # enabled, create a new one.
     proxy %>% clearControls()
-    if (input$legend) {
-      pal <- colorpal()
-      proxy %>% addLegend(position = "bottomright",
-                          pal = pal, values = ~mag
-      )
-    }
+    pal <- colorpal()
+    proxy %>% addLegend(position = "bottomright",
+                        pal = pal, values = ~rating
+    )
   })
 }
 
