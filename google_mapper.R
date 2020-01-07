381da8b14dd6ea31694b456acf6ece3129619400
schackartk <schackartk@email.arizona.edu>
2020-01-07 16:05:53 -0700

    Remove regex in ratings. Begin labeled places

--- google_mapper.R
+++ google_mapper.R
@@ -7,6 +7,8 @@ library(htmlwidgets) # Saving HTML widget as .html file
 
 ratings_file <-"Takeout/Reviews.json" 
 loc_hist_file <- "Takeout/LocationHistory.json"
+label_file <- "Takeout/LabeledPlaces.json"
+saved_file <- "Takeout/SavePlaces.json"
 
 
 # Functions ---------------------------------------------------------------
@@ -17,68 +19,71 @@ tidy_json <- function(f) {
   return(out)
 }
 
-clean_ratings <- function(ratings_tibble) {
+clean_ratings <- function(df) {
   
-  ratings_tibble <- ratings_tibble %>% 
+  # Filter out unnecessary columns
+  df <- df %>% 
     select(-type, -features.type, -features.geometry.coordinates, 
                             -features.geometry.type, -`features.properties.Location.Country Code`)
  
    # Rename columns to make more sense
-  colnames(ratings_tibble)[1] <- "maps_url"
-  colnames(ratings_tibble)[2] <- "date_time"
-  colnames(ratings_tibble)[3] <- "rating"
-  colnames(ratings_tibble)[4] <- "review"
-  colnames(ratings_tibble)[5] <- "address"
-  colnames(ratings_tibble)[6] <- "business_name"
-  colnames(ratings_tibble)[7] <- "latitude"
-  colnames(ratings_tibble)[8] <- "longitude"
+  colnames(df)[1] <- "maps_url"
+  colnames(df)[2] <- "date_time"
+  colnames(df)[3] <- "rating"
+  colnames(df)[4] <- "review"
+  colnames(df)[5] <- "address"
+  colnames(df)[6] <- "business_name"
+  colnames(df)[7] <- "latitude"
+  colnames(df)[8] <- "longitude"
   
   # Fix time formatting and data type
-  ratings_tibble$date_time <- ratings_tibble$date_time %>% 
+  df$date_time <- df$date_time %>% 
     gsub(pattern = "T", replacement = " ")  %>% 
     gsub(pattern = "Z", replacement = "") %>% ymd_hms()
   
   # Change $rating column to factor
-  ratings_tibble$rating <- as.factor(ratings_tibble$rating) %>% 
+  df$rating <- as.factor(df$rating) %>% 
     factor(levels = c("1", "2", "3", "4", "5"))
   
-  # Parse out parts of the address column using regular expressions via stringr::str_match
-  addresses <- str_match(ratings_tibble$address,
-                         "[[:digit:]]+[[:space:]][[:alpha:]]{1}[[:space:]][[:alnum:][:space:]]+") # e.g. "21 N Squaw Creek Dr"
-  cities <- str_match(ratings_tibble$address,"[,][[:space:]][[:alpha:]]+[,]") %>% # e.g. ", Bronx,"
-    gsub(pattern = ",",replacement = "") %>% trimws() %>% as.vector()
-  state.zip <- str_match(ratings_tibble$address,"([[:alpha:]]{2})[[:space:]]([[:digit:]]{5})") # e.g. "NY 10469"
-  states <- state.zip[,2]
-  zips <- state.zip[,3]
-  countries <- str_match(ratings_tibble$address,"[[:alpha:][:space:]]+$") %>% # e.g. "United States" (anchored to end)
-    gsub(pattern = "USA", replacement = "United States") %>% trimws()
-  
-  # Add in the columns of the parsed address column
-  ratings_tibble <- ratings_tibble %>% 
-    add_column(states, zips, addresses, cities, countries)
-  
-  # Remove the old, unparsed, address column
-  ratings_tibble <- ratings_tibble %>% select(-address)
-  
   # Convert latitude and longitude to numeric type
-  ratings_tibble$latitude <- as.numeric(ratings_tibble$latitude)
-  ratings_tibble$longitude <- as.numeric((ratings_tibble$longitude))
+  df$latitude <- as.numeric(df$latitude)
+  df$longitude <- as.numeric((df$longitude))
   
-  return(ratings_tibble)
+  return(df)
 }
 
-clean_history <- function(loc_hist_tibble) {
-  loc_hist_tibble <- loc_hist_tibble %>% 
+clean_history <- function(df) {
+  df <- df %>% 
     select(locations.latitudeE7, locations.longitudeE7)
   
-  colnames(loc_hist_tibble)[1] <- "latitude"
-  colnames(loc_hist_tibble)[2] <- "longitude"
+  colnames(df)[1] <- "latitude"
+  colnames(df)[2] <- "longitude"
   
   # Get rid of NA values and rescale
-  loc_hist_tibble <- na.omit(loc_hist_tibble)
-  loc_hist_tibble <- loc_hist_tibble*10^-7 # These are originally recorded as *10^7
+  df <- na.omit(df)
+  df <- df*10^-7 # These are originally recorded as *10^7
   
-  return(loc_hist_tibble)
+  return(df)
+}
+
+clean_labels <- function(df) {
+  df <- df %>% 
+    select(-type, -features.type, -features.geometry.type)
+  
+  colnames(df)[1] <- "coords"
+  colnames(df)[2] <- "address"
+  colnames(df)[3] <- "name"
+  
+  latitude <- str_match(df$coords, "[[:digit:]]+\\.[[:digit:]]+[,]") %>% 
+    gsub(pattern = ",", replacement = "")
+  
+  longitude <- str_match(df$coords, ".[[:digit:]]+\\.[[:digit:]]+[\\)]") %>% 
+    gsub(pattern = ")", replacement = "") %>% trimws()
+  
+  df <- df %>% 
+    add_column(latitude, longitude) %>% select(-coords)
+  
+  return(df)
 }
 
 map_ratings <- function(df, m) {
@@ -123,6 +128,8 @@ if (file.exists(ratings_file)) {
   cleaned_ratings <- clean_ratings(ratings)
   my_map <- map_ratings(cleaned_ratings, my_map)
   layers <- append(layers, "Ratings")
+} else {
+  message(paste0("No file found for ratings: '",ratings_file,"'"))
 }
 
 if (file.exists(loc_hist_file)) {
@@ -130,6 +137,13 @@ if (file.exists(loc_hist_file)) {
   cleaned_loc_hist <- clean_history(loc_hist)
   my_map <- map_loc_hist(cleaned_loc_hist, my_map)
   layers <- append(layers, "Heatmap")
+} else {
+  message(paste0("No file found for location history: '",loc_hist_file, "'"))
+}
+
+if(file.exists(label_file)) {
+  labs <- tidy_json(label_file)
+  cleaned_labs <- clean_labels(labs)
 }
 
 my_map <- my_map %>% addLayersControl(
