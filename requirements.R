403ab8832b9e29efb086928de77c3a9a5c4f5569
schackartk <schackartk@email.arizona.edu>
2020-01-16 09:17:18 -0700

    Update libraries

--- requirements.R
+++ requirements.R
@@ -1,5 +1,7 @@
 install.packages("tidyverse")
 install.packages("leaflet")
+install.packages("leaflet.extras")
 install.packages("lubridate")
 install.packages("jsonlite")
 install.packages("htmlwidgets")
+install.packages("argparser")
