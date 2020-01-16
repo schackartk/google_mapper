2c430d96756588f6fe6f69fecef472bfd4578496
schackartk <schackartk@email.arizona.edu>
2020-01-16 09:16:53 -0700

    Add executable requirements script

---
+++ exec_requirements.R
@@ -0,0 +1,9 @@
+#!/usr/bin/env Rscript
+
+install.packages("tidyverse")
+install.packages("leaflet")
+install.packages("leaflet.extras")
+install.packages("lubridate")
+install.packages("jsonlite")
+install.packages("htmlwidgets")
+install.packages("argparser")
