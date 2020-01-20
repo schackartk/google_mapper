6b683ce7b79c3bc9660c56ece5c9d372403aa284
schackartk <schackartk@email.arizona.edu>
2020-01-20 12:39:16 -0700

    Loop through files and add better error handling

--- google_mapper.R
+++ google_mapper.R
@@ -16,79 +16,117 @@ map_data <- function(files) {
   # Functions ---------------------------------------------------------------
   
   tidy_json <- function(f) {
-    out <- fromJSON(f) %>% 
-      as_tibble() %>% flatten() %>% as_tibble()
+    # Try to parse file as json, if it fails, return NA
+    out <- tryCatch({
+      fromJSON(f) %>% as_tibble() %>% flatten() %>% as_tibble()
+    },
+    error = function(cond) {
+      message(paste("\nUnable to interpret file:", f, "as '.json'... Skipping."))
+      return(NA)
+    }
+    )
     return(out)
   }
   
-  clean_ratings <- function(df) {
+  clean_ratings <- function(df, f) {
     
-    # Filter out unnecessary columns
-    df <- df %>% 
-      select(-type, -features.type, -features.geometry.coordinates, 
-             -features.geometry.type, -`features.properties.Location.Country Code`)
     
-    # Rename columns to make more sense
-    colnames(df)[1] <- "maps_url"
-    colnames(df)[2] <- "date_time"
-    colnames(df)[3] <- "rating"
-    colnames(df)[4] <- "review"
-    colnames(df)[5] <- "address"
-    colnames(df)[6] <- "business_name"
-    colnames(df)[7] <- "latitude"
-    colnames(df)[8] <- "longitude"
-    
-    # Fix time formatting and data type
-    df$date_time <- df$date_time %>% 
-      gsub(pattern = "T", replacement = " ")  %>% 
-      gsub(pattern = "Z", replacement = "") %>% ymd_hms()
-    
-    # Change $rating column to factor
-    df$rating <- as.factor(df$rating) %>% 
-      factor(levels = c("1", "2", "3", "4", "5"))
-    
-    # Convert latitude and longitude to numeric type
-    df$latitude <- as.numeric(df$latitude)
-    df$longitude <- as.numeric((df$longitude))
-    
-    return(df)
+    out <- tryCatch({
+      # Filter out unnecessary columns
+      df <- df %>% 
+        select(-type, -features.type, -features.geometry.coordinates, 
+               -features.geometry.type, -`features.properties.Location.Country Code`)
+      
+      # Rename columns to make more sense
+      colnames(df)[1] <- "maps_url"
+      colnames(df)[2] <- "date_time"
+      colnames(df)[3] <- "rating"
+      colnames(df)[4] <- "review"
+      colnames(df)[5] <- "address"
+      colnames(df)[6] <- "business_name"
+      colnames(df)[7] <- "latitude"
+      colnames(df)[8] <- "longitude"
+      
+      # Fix time formatting and data type
+      df$date_time <- df$date_time %>% 
+        gsub(pattern = "T", replacement = " ")  %>% 
+        gsub(pattern = "Z", replacement = "") %>% ymd_hms()
+      
+      # Change $rating column to factor
+      df$rating <- as.factor(df$rating) %>% 
+        factor(levels = c("1", "2", "3", "4", "5"))
+      
+      # Convert latitude and longitude to numeric type
+      df$latitude <- as.numeric(df$latitude)
+      df$longitude <- as.numeric((df$longitude))
+      
+      df
+    },
+    error = function(cond) {
+      message(paste("\nUnable to interpret file", f, "in context as ratings file."))
+      message(paste("Please ensure that you meant this file to be interpreted as ratings/reviews."))
+      message("Skipping.")
+      return(NA)
+    })
+    return(out)
   }
   
-  clean_history <- function(df) {
-    df <- df %>% 
-      select(locations.latitudeE7, locations.longitudeE7)
-    
-    colnames(df)[1] <- "latitude"
-    colnames(df)[2] <- "longitude"
-    
-    # Get rid of NA values and rescale
-    df <- na.omit(df)
-    df <- df*10^-7 # These are originally recorded as *10^7
-    
-    return(df)
+  clean_history <- function(df, f) {
+    out <- tryCatch({
+      df <- df %>% 
+        select(locations.latitudeE7, locations.longitudeE7)
+      
+      colnames(df)[1] <- "latitude"
+      colnames(df)[2] <- "longitude"
+      
+      # Get rid of NA values and rescale
+      df <- na.omit(df)
+      df <- df*10^-7 # These are originally recorded as *10^7
+      
+      df
+    },
+    error = function(cond) {
+      message(paste("\nUnable to interpret file", f, "in context as location history file."))
+      message(paste("Please ensure that you meant this file to be interpreted as location history."))
+      message("Skipping.")
+      return(NA)
+    })
+    return(out)
   }
   
-  clean_labels <- function(df) {
-    df <- df %>% 
-      select(-type, -features.type, -features.geometry.type)
+  clean_labels <- function(df, f) {
     
-    colnames(df)[1] <- "coords"
-    colnames(df)[2] <- "address"
-    colnames(df)[3] <- "name"
+    out <- tryCatch({
+      df <- df %>% 
+        select(features.geometry.coordinates, features.properties.address, features.properties.name)
+      
+      colnames(df)[1] <- "coords"
+      colnames(df)[2] <- "address"
+      colnames(df)[3] <- "name"
+      
+      latitude <- str_match(df$coords, "[[:digit:]]+\\.[[:digit:]]+[,]") %>% 
+        gsub(pattern = ",", replacement = "")
+      
+      longitude <- str_match(df$coords, ".[[:digit:]]+\\.[[:digit:]]+[\\)]") %>% 
+        gsub(pattern = ")", replacement = "") %>% trimws()
+      
+      df <- df %>% 
+        add_column(latitude, longitude) %>% select(-coords)
+      
+      df$latitude <- as.numeric(df$latitude)
+      df$longitude <- as.numeric(df$longitude)
+      
+      df
+    },
+    error = function(cond) {
+      message(paste("\nUnable to interpret file", f, "in context as labeled places file."))
+      message(paste("Please ensure that you meant this file to be interpreted as labeled places."))
+      message("Skipping.")
+      return(NA)
+    })
     
-    latitude <- str_match(df$coords, "[[:digit:]]+\\.[[:digit:]]+[,]") %>% 
-      gsub(pattern = ",", replacement = "")
     
-    longitude <- str_match(df$coords, ".[[:digit:]]+\\.[[:digit:]]+[\\)]") %>% 
-      gsub(pattern = ")", replacement = "") %>% trimws()
-    
-    df <- df %>% 
-      add_column(latitude, longitude) %>% select(-coords)
-    
-    df$latitude <- as.numeric(df$latitude)
-    df$longitude <- as.numeric(df$longitude)
-    
-    return(df)
+    return(out)
   }
   
   map_ratings <- function(df, m) {
@@ -138,37 +176,52 @@ map_data <- function(files) {
   
   # Add layers for files that are present
   
-  if (file.exists(ratings_file)) {
-    message("Processing ratings file... ", appendLF = FALSE)
-    ratings <- tidy_json(ratings_file)
-    cleaned_ratings <- clean_ratings(ratings)
-    my_map <- map_ratings(cleaned_ratings, my_map)
-    layers <- append(layers, "Ratings")
-    message("Done processing ratings file.")
-  } else {
-    message(paste0("No file found for ratings: '", ratings_file,"'. Skipping."))
-  }
+  file_labels <- c("ratings", "location history", "labeled places")
+  layer_labels <- c("Ratings", "Location Heatmap", "Labeled Places")
+  file_names <- c(ratings_file, loc_hist_file, label_file)
+  names(layer_labels) <- file_labels
+  names(file_names) <- file_labels
   
-  if (file.exists(loc_hist_file)) {
-    message("Processing location history file... ", appendLF = FALSE)
-    loc_hist <- tidy_json(loc_hist_file)
-    cleaned_loc_hist <- clean_history(loc_hist)
-    my_map <- map_loc_hist(cleaned_loc_hist, my_map)
-    layers <- append(layers, "Location Heatmap")
-    message("Done processing location history file.")
-  } else {
-    message(paste0("No file found for location history: '", loc_hist_file, "'. Skipping."))
-  }
-  
-  if(file.exists(label_file)) {
-    message("Processing labeled places file... ", appendLF = FALSE)
-    labs <- tidy_json(label_file)
-    cleaned_labs <- clean_labels(labs)
-    my_map <- map_labels(cleaned_labs, my_map)
-    layers <- append(layers, "Labeled Places")
-    message("Done processing labeled places file.")
-  }else {
-    message(paste0("No file found for labeled places: '", label_file, "'. Skipping."))
+  for(fn in file_labels) {
+    
+    # Check that there is a file at the given path
+    if (file.exists(file_names[[fn]])) {
+      message(paste("Processing", fn, "file... "), appendLF = FALSE)
+      raw_data <- tidy_json(file_names[[fn]])
+      
+      # Check that data parsing went ok
+      suppressWarnings(if(!is.na(raw_data)) {
+        # If the file could be parsed as a json file, I am going to assume it is in the right format
+        
+        # Work with and map each data set with its own treatment
+        if(fn == "ratings") {
+          cleaned_ratings <- clean_ratings(raw_data, file_names[[fn]])
+          # If data could be interpreted in context, plot it.
+          suppressWarnings(if(!is.na(cleaned_ratings)) {
+            my_map <- map_ratings(cleaned_ratings, my_map)
+            layers <- append(layers, layer_labels[[fn]])
+          })
+        } else if(fn == "location history") {
+          cleaned_loc_hist <- clean_history(raw_data, file_names[[fn]])
+          suppressWarnings(if(!is.na(cleaned_loc_hist)) {
+            my_map <- map_loc_hist(cleaned_loc_hist, my_map)
+            layers <- append(layers, layer_labels[[fn]])
+          })
+        } else if(fn == "labeled places") {
+          cleaned_labs <- clean_labels(raw_data, file_names[[fn]])
+          suppressWarnings(if(!is.na(cleaned_labs)) {
+            my_map <- map_labels(cleaned_labs, my_map)
+            layers <- append(layers, layer_labels[[fn]])
+          })
+        }
+        
+        message(paste("Done processing", fn, "file."))
+      }
+      )
+    } else {
+      message(paste0("No ", fn, " file found: '", file_names[[fn]],"'. Skipping."))
+    }
+    
   }
   
   # Check that layers are present
@@ -188,6 +241,6 @@ map_data <- function(files) {
     
     message(paste0("Done, see file: '", out_file, "'!"))
   }
-
+  
 }
 
