900921b3b4e8eddee4d0b32060fcd1792d171736
schackartk <schackartk@email.arizona.edu>
2020-01-20 15:41:19 -0700

    Trying to make appearance on mobile better. No luck

--- google_mapper.R
+++ google_mapper.R
@@ -1,3 +1,6 @@
+# Author: Kenneth Schackart
+#         schackartk1@gmail.com
+
 suppressWarnings(suppressMessages(library(tidyverse))) # Of course
 suppressWarnings(suppressMessages(library(leaflet))) # For making beautiful maps
 suppressWarnings(suppressMessages(library(leaflet.extras))) # Necessary for heatmap()
@@ -174,10 +177,11 @@ map_data <- function(files) {
   
   # Initialize an empty map
   
-  my_map <- leaflet() %>% addTiles(group = "OSM (default)", attribution = paste0("Source code: <a href='https://github.com/schackartk/google_mapper' target= '_blank'",
-                                                                                 " title = 'google_mapper GitHub repository' >google_mapper</a>,",
-                                                                                 " by <a href='https://twitter.com/SchackartK' target= '_blank'",
-                                                                                 " title = 'Author twitter' >@SchackartK</a>"))
+  my_map <- leaflet() %>%
+    addTiles(group = "OSM (default)", attribution = paste0("Source code: <a href='https://github.com/schackartk/google_mapper' target= '_blank'",
+                                                           " title = 'google_mapper GitHub repository' >google_mapper</a>,",
+                                                           " by <a href='https://twitter.com/SchackartK' target= '_blank'",
+                                                           " title = 'Author twitter' >@SchackartK</a>"))
   layers <- NULL
   
   # Add layers for files that are present
